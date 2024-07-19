d627023dd57e72a314e6a95834398d4afe197b9d
Dom Bryan <dbryan@contractor.indeed.com>
2024-07-19 22:10:52 +0100

    Initial Commit

---
+++ SpaceIsCool/SpaceIsCoolApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpaceIsCoolApp.swift
+//  SpaceIsCool
+//
+//  Created by Dom Bryan on 19/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct SpaceIsCoolApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
